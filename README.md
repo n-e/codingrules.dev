1052a1df077344fb744d1207f50c94cf6de1723e
Nicolas Even <n-e@users.noreply.github.com>
2022-03-13 19:28:58 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Source Code and rules for [codingrules.dev](https://www.codingrules.dev)
+
+## Contributing
+
+TODO
+
+## License
+
+TODO
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.{tsx,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
